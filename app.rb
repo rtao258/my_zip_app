f8075153f303f3bd4601fb7296367e59cde59c1a
Raymond Tao <rtao@web02.hpc.osc.edu>
2018-06-15 11:40:32 -0400

    checks file path for existence

--- app.rb
+++ app.rb
@@ -10,4 +10,15 @@ class App < Sinatra::Base
   get "/" do
     erb :index
   end
+  
+  post "/" do
+    @path = Pathname.new(params[:file]).expand_path
+    if @path.exist?
+      redirect to("/")
+    else
+      @error = "The path '#{@path}' does not exist."
+    end
+    erb :index
+  end
+  
 end
